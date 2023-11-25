b3c134ee4e9ff40ad384857b670750e60bdb6d98
jeremiah mathayo <mathsjery05@gmail.com>
2023-11-25 12:58:57 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './Weather';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Weathe-App</h1>
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
